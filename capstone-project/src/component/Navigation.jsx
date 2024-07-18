2d2e692c0d06936f72a9abeab8a7f75443257e7f
Reena kumari <reenakvs1@gmail.com>
2024-07-18 19:57:06 +0530

    kjfhj

--- capstone-project/src/component/Navigation.jsx
+++ capstone-project/src/component/Navigation.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Navigation() {
   return (
-    <div>Navigation</div>
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/menu">Menu</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+    </nav>
   )
 }
