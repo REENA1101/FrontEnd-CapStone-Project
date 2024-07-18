b069d311032c6c088a3487955174ef45b0ec0655
Reena kumari <reenakvs1@gmail.com>
2024-07-18 20:18:48 +0530

    kefhiuerf

--- capstone-project/src/App.js
+++ capstone-project/src/App.js
@@ -4,15 +4,23 @@ import Header from './component/Header';
 import Main from './component/Main';
 import Footer from './component/Footer';
 import Navigation from './component/Navigation';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <>
-       <h1>CAPSTONE PROJECT</h1>   
-       <Header />
-       <Navigation />
-       <Main />
-       <Footer />
+       <h1>CAPSTONE PROJECT</h1>  
+        <Helmet>
+          <meta name="description" content="Little Lemon is a charming restaurant offering delicious food and exceptional service." />
+          <meta property="og:title" content="Little Lemon - Your Favorite Restaurant" />
+          <meta property="og:description" content="Experience the taste of Little Lemon. Book your table now!" />
+          <meta property="og:image" content="https://example.com/little-lemon-logo.png" />
+        </Helmet> 
+
+          <Header />
+          <Navigation />
+          <Main />
+          <Footer />
     </>
   );
 }
