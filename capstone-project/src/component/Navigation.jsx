03e12ec0549010226492a3918205cd1a872bceef
Reena kumari <reenakvs1@gmail.com>
2024-07-25 14:49:04 +0530

    navbara scroll done

--- capstone-project/src/component/Navigation.jsx
+++ capstone-project/src/component/Navigation.jsx
@@ -1,21 +1,42 @@
-import React from 'react'
-import Header from '../component/Header'
+import React, { useState, useEffect } from 'react';
+import Header from '../component/Header';
 
-export default function Navigation() {
+const Navigation = () => {
+  const [showNavbar, setShowNavbar] = useState(true);
+  const scrollThreshold = 100; // Customizable scroll threshold
+
+  const handleScroll = () => {
+    if (window.scrollY > scrollThreshold) {
+      setShowNavbar(false);
+    } else {
+      setShowNavbar(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-        <nav >
-            <ul >
-                <Header />
-                <div className="search-container">
-                  <input type="text" placeholder="Search"></input>
-                  {/* <button type="submit"><i class="fa fa-search"></i></button> */}
-                </div>
-                <li ><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/bookingPage">Bookings</a></li>
-                <li ><a href="/contact">Contact</a></li>
-                <li ><a href="/chicago">Chicago</a></li>
-            </ul>
-       </nav>
-  )
-}
+    <nav className={`navbar ${showNavbar ? 'navbar-visible' : 'navbar-hidden'}`}>
+      <ul >
+          <Header />
+              <div className="search-container">
+                <input type="text" placeholder="Search"></input>
+                {/* <button type="submit"><i class="fa fa-search"></i></button> */}
+              </div>
+                  <li ><a href="/">Home</a></li>
+                  <li><a href="/about">About</a></li>
+                  <li><a href="/bookingPage">Bookings</a></li>
+                  <li ><a href="/contact">Contact</a></li>
+                  <li ><a href="/chicago">Chicago</a></li>
+                  </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
+
+
+
