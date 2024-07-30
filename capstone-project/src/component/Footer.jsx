b507ddc73fe8801e31947ab11368ed74da60d740
Reena kumari <reenakvs1@gmail.com>
2024-07-31 00:19:55 +0530

    jbfjbf

--- capstone-project/src/component/Footer.jsx
+++ capstone-project/src/component/Footer.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 
+
 export default function Footer() {
   return (
     <footer className="footer">
-        <p>&copy; Little Lemon 2023</p>
-        {/* Other footer content */}
+        <div className='footerDivOne'>
+        <div className='footerDivDivOne'>
+               <img src="../images/footer.png" alt="logoPhoto"/>
+              </div>
+
+              <div className='footerDivDivTwo'>
+                 <a>Home</a>
+                 <a>About Us</a>
+                 <a>Book Your Spot Now</a>
+                 <a>Contact US</a>
+                 <a>Our Chicago Branch</a>
+              </div>
+
+              <div className='footerDivDivThree'>
+                 <text> Contact Us </text>
+              </div>
+
+              <div className='footerDivDivFour'>
+                 <text></text>
+              </div>
+        </div>
+
+
+          <p> &copy; Little Lemon 2023</p>
+
+
     </footer>
   )
 }
