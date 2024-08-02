708ac054b8507b5acf425f7d7e6fe38474ce561b
Reena kumari <reenakvs1@gmail.com>
2024-08-03 00:48:17 +0530

    jfjsb

--- capstone-project/src/component/Footer.jsx
+++ capstone-project/src/component/Footer.jsx
@@ -13,27 +13,40 @@ export default function Footer() {
               </div>
 
               <div className='footerDivDivTwo'>
-                  <ul>
-                     <li><h3>Site Links</h3></li>
-                     <li><a>Home</a></li>
-                     <li> <a>About Us</a></li>
-                     <li><a>Book Your Spot Now</a></li>
-                     <li><a>Contact US</a></li>
-                     <li><a>Our Chicago Branch</a></li>
+              <h2 style={{fontSize:'24px', marginBottom:"10px"}}>Site Links</h2>
+                  <ul style={{listStyleType: 'none'}}>
+                     <li><a href="#">Home</a></li>
+                     <li> <a href="#">About Us</a></li>
+                     <li><a href="#">Book Your Spot Now</a></li>
+                     <li><a href="#">Contact US</a></li>
+                     <li><a href="#">Our Chicago Branch</a></li>
                   </ul>
               </div>
 
-              <div className='footerDivDivThree'>
-               <ul>
-               <li> Contact Us </li>
+              <div className='footerDivDivFour'>
+              <h2 style={{fontSize:'24px', marginBottom:"10px"}}> Our Restaurants In </h2>
+              <ul style={{listStyleType: 'none'}}>
+                  <li><a>Chicago</a></li>
+                  <li><a>Tokyo</a></li>
+                  <li><a>Canada</a></li>
+                  <li><a>Paris</a></li>
+                  <li><a>USA</a></li>
                </ul>
               </div>
 
-              <div className='footerDivDivFour'>
-                 <text></text>
+              <div className='footerDivDivThree'>
+              <h2 style={{fontSize:'24px', marginBottom:"10px"}}> Contact Us </h2>
+               <ul style={{listStyleType: 'none'}}>
+                  <a><li>Instagram</li></a>
+                  <a><li>facebook</li></a>
+                  <a><li>Youtube</li></a>
+                  <a><li>Dribble</li></a>
+                  <li>...</li>
+               </ul>
               </div>
+
               </div>
-              <p> &copy; Little Lemon 2023</p>
+              <p> &copy; Little Lemon 2024</p>
 
               </div>
 
