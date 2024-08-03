43289170094349e112240c728b668ad593ee39f1
Reena kumari <reenakvs1@gmail.com>
2024-08-03 19:14:06 +0530

    bhcfjhcb

--- capstone-project/src/component/LandingPage.jsx
+++ capstone-project/src/component/LandingPage.jsx
@@ -7,14 +7,18 @@ import BackToTopButton from './FunctionalComps/BackToTopButton';
 export default function LandingPage() {
   return (
     <div className="landingpage" style={{ marginLeft: '100px', marginRight: '40px'}}>
+         <div className='main2'>
+            <h1>LITTLE </h1>
+            <h1>LEMON RESTAURANT</h1>
+            <h1>CHICAGO</h1>
+          </div>
+
+
 
         <FoodSlider />
 
         <img src={main1} alt="Main1" />
-          <div className='main2'>
-            <h1>LITTLE </h1>
-            <h1>LEMON RESTAURANT</h1>
-          </div>
+         
 
         <img src={main2} alt="Main2"/>
         <BackToTopButton/>
