a83a437fe0072d1b95fe32138b0ef22fce05dceb
Reena kumari <reenakvs1@gmail.com>
2024-08-04 00:30:08 +0530

    jagfhg

--- capstone-project/src/component/LandingPage.jsx
+++ capstone-project/src/component/LandingPage.jsx
@@ -3,22 +3,27 @@ import main1 from '../images/main1.png';
 import main2 from "../images/main2.png";
 import FoodSlider from './FunctionalComps/FoodSlider';
 import BackToTopButton from './FunctionalComps/BackToTopButton';
+import chef from "../images/ChefPhoto.jpg"
 
 export default function LandingPage() {
   return (
-    <div className="landingpage" style={{ marginLeft: '100px', marginRight: '40px'}}>
-         <div className='main2'>
-            <h1>LITTLE </h1>
-            <h1>LEMON RESTAURANT</h1>
-            <h1>CHICAGO</h1>
+    <div className="landingpage" style={{ marginLeft: '105px', marginRight: '40px'}}>
+         <div className='main2' style={{display:"flex", marginBottom:"40px",  justifyContent:"space-between", backgroundColor: 'white', height: '400px', borderRadius: '8px'}}>
+              <div>
+                <h1>LITTLE </h1>
+                <h1>LEMON RESTAURANT</h1>
+                <h1>CHICAGO</h1>
+                <button style={{backgroundColor:"#353333",color:"white", padding:"10px"}}>Reserve Your Table</button>
+                </div>
+            <div>
+              <img style={{width:"400px"}} src={chef} alt="chefPhoto"/>
+              </div>
           </div>
-
-
 
         <FoodSlider />
 
         <img src={main1} alt="Main1" />
-         
+
 
         <img src={main2} alt="Main2"/>
         <BackToTopButton/>
