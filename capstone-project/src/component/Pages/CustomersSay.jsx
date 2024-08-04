e2ab5bc33fe3012793cbc423e3f76136ecc39804
Reena kumari <reenakvs1@gmail.com>
2024-08-04 17:24:23 +0530

    hdjhfb

--- capstone-project/src/component/Pages/CustomersSay.jsx
+++ capstone-project/src/component/Pages/CustomersSay.jsx
@@ -14,11 +14,9 @@ export default function CustomersSay() {
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
         fill={i < rating ? "gold" : "gray"}
-        width="18px"
-        height="18px"
-        display="flex"
-        justify-content="center"
-        margin="-30px"
+        width="20px"
+        height="20px"
+       
       >
         <path d="M12 17.27L18.18 21 16.54 13.97 22 9.24 14.81 8.63 12 2 9.19 8.63 2 9.24 7.46 13.97 5.82 21z"/>
       </svg>
@@ -36,7 +34,9 @@ export default function CustomersSay() {
         <h3>Marco Fernandez
         <div className="stars">{renderStars(5)}</div>
         </h3>
-        <h4>The food is exquisite, different wonderful cuisines at one place</h4>
+        <h4>"Little Lemon offers a delightful dining experience with its fresh and flavorful dishes.
+           The lemon herb chicken was absolutely divine, bursting with vibrant citrus notes. The cozy
+            ambiance and attentive service make it a must-visit spot!"</h4>
 
       </div>
       <div className='custItem'>
@@ -44,7 +44,9 @@ export default function CustomersSay() {
         <h3>Rob Williams
         <div className="stars">{renderStars(4)}</div>
         </h3>
-        <h4>The Online Booking system is very efficient.</h4>
+        <h4>"A hidden gem in the heart of the city! The seafood pasta was a standout,
+           perfectly cooked and seasoned. Great atmosphere and friendly staff, though 
+           the wait time was a bit long."</h4>
 
       </div>
       <div className='custItem'>
@@ -52,7 +54,9 @@ export default function CustomersSay() {
         <h3>Lisa Hopkins
         <div className="stars">{renderStars(5)}</div>
         </h3>
-        <h4>Good Food Good vibe a must visit recommmended.</h4>
+        <h4>"Good Food Good Vibe, a must visit, highly recommended. The grilled salmon with 
+          lemon butter sauce was simply outstanding. Friendly staff and warm atmosphere
+           make it a great spot for any occasion!"</h4>
 
       </div>
     </div>
