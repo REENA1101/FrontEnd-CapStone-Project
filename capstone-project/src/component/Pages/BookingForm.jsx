3085e1d356b5c67f57bf8cfafa43cd2094d1086a
Reena kumari <reenakvs1@gmail.com>
2024-08-04 10:00:49 +0530

    hbchjdcb

--- capstone-project/src/component/Pages/BookingForm.jsx
+++ capstone-project/src/component/Pages/BookingForm.jsx
@@ -12,46 +12,82 @@ const BookingForm = ({ selectedDate, setSelectedDate, availabledTimes, dispatch,
   const [numberOfGuests, setNumberOfGuests] = useState(1);
   const [occasion, setOccasion] = useState('');
   const [showNotification, setShowNotification] = useState(false);
-  const [notificationMessage, setNotificationMessage] = useState('');
+  // const [notificationMessage, setNotificationMessage] = useState('');
+  const [dateError, setDateError] = useState('');
+  const [timeError, setTimeError] = useState('');
+  const [guestsError, setGuestsError] = useState('');
+  const [occasionError, setOccasionError] = useState('');
+
   const availableTimes = ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
+    // if (!reservationDate) {
+    //   setNotificationMessage('Please select a date.');
+    //   setShowNotification(true);
+    //   setTimeout(() => setShowNotification(false), 4000);
+    //   return;
+    // }
+
+    // if (!reservationTime) {
+    //   setNotificationMessage('Please select a time.');
+    //   setShowNotification(true);
+    //   setTimeout(() => setShowNotification(false), 4000);
+    //   return;
+    // }
+
+    // if (!numberOfGuests || numberOfGuests < 1) {
+    //   setNotificationMessage('Please enter a valid number of guests.');
+    //   setShowNotification(true);
+    //   setTimeout(() => setShowNotification(false), 4000);
+    //   return;
+    // }
+
+    // if (!occasion) {
+    //   setNotificationMessage('Please select an occasion.');
+    //   setShowNotification(true);
+    //   setTimeout(() => setShowNotification(false), 4000);
+    //   return;
+    // }
+
+    let valid=true;
+
     if (!reservationDate) {
-      setNotificationMessage('Please select a date.');
-      setShowNotification(true);
-      setTimeout(() => setShowNotification(false), 4000);
-      return;
+      setDateError('Please select a date.');
+      valid = false;
+    } else {
+      setDateError('');
     }
 
     if (!reservationTime) {
-      setNotificationMessage('Please select a time.');
-      setShowNotification(true);
-      setTimeout(() => setShowNotification(false), 4000);
-      return;
+      setTimeError('Please select a time.');
+      valid = false;
+    } else {
+      setTimeError('');
     }
 
     if (!numberOfGuests || numberOfGuests < 1) {
-      setNotificationMessage('Please enter a valid number of guests.');
-      setShowNotification(true);
-      setTimeout(() => setShowNotification(false), 4000);
-      return;
+      setGuestsError('Please enter a valid number of guests.');
+      valid = false;
+    } else {
+      setGuestsError('');
     }
 
     if (!occasion) {
-      setNotificationMessage('Please select an occasion.');
-      setShowNotification(true);
-      setTimeout(() => setShowNotification(false), 4000);
-      return;
+      setOccasionError('Please select an occasion.');
+      valid = false;
+    } else {
+      setOccasionError('');
     }
 
 
-    // Handle form submission logic here
-    console.log(reservationDate, reservationTime, numberOfGuests, occasion);
-    setShowNotification(true);
-    setTimeout(() => setShowNotification(false), 4000);
-
+     if(valid){
+        // Handle form submission logic here
+        console.log(reservationDate, reservationTime, numberOfGuests, occasion);
+        setShowNotification(true);
+        setTimeout(() => setShowNotification(false), 4000);
+     }
   };
 
   return (
@@ -64,6 +100,8 @@ const BookingForm = ({ selectedDate, setSelectedDate, availabledTimes, dispatch,
         value={reservationDate}
         onChange={(e) => setReservationDate(e.target.value)}
       />
+        {dateError && <div style={{ color: 'red' }}>{dateError}</div>}
+
       <label htmlFor="res-time">Choose time</label>
       <select
         id="res-time"
@@ -77,6 +115,9 @@ const BookingForm = ({ selectedDate, setSelectedDate, availabledTimes, dispatch,
           </option>
         ))}
       </select>
+      {timeError && <div style={{ color: 'red' }}>{timeError}</div>}
+
+
       <label htmlFor="guests">Number of guests</label>
       <input
         type="number"
@@ -87,6 +128,9 @@ const BookingForm = ({ selectedDate, setSelectedDate, availabledTimes, dispatch,
         value={numberOfGuests}
         onChange={(e) => setNumberOfGuests(Number(e.target.value))}
       />
+
+{guestsError && <div style={{ color: 'red' }}>{guestsError}</div>}
+
       <label htmlFor="occasion">Occasion</label>
       <select
         id="occasion"
@@ -97,14 +141,18 @@ const BookingForm = ({ selectedDate, setSelectedDate, availabledTimes, dispatch,
         <option value="Birthday">Birthday</option>
         <option value="Anniversary">Anniversary</option>
       </select>
-      <input type="submit" value="Make Your Reservation" />
+      {occasionError && <div style={{ color: 'red' }}>{occasionError}</div>}
 
+
+
+      <input type="submit" value="Make Your Reservation" />
     </form>
 
     {showNotification && (
-        <div style={{ marginTop: '-220px', marginLeft:"485px",  padding: '36px', backgroundColor: '#353333', color: '#cacaca', width:"300px",height:"120px", zIndex:"100", borderRadius: '5px',textAlign:"center", position: 'absolute', fontSize:"20px" }}>
+        <div style={{ marginTop: '-220px', marginLeft:"485px",  padding: '30px', backgroundColor: '#353333', color: '#cacaca', width:"300px",height:"120px", zIndex:"100", borderRadius: '5px',textAlign:"center", position: 'absolute', fontSize:"20px" }}>
           Thank you for your reservation!
-          {{notificationMessage}}
+          {/* {{notificationMessage}} */}
+          
         </div>
       )}
 
