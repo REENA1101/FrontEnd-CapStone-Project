11a11c978fc4521a31338c9e6c348f8386dfd05f
Reena kumari <reenakvs1@gmail.com>
2024-08-04 17:45:26 +0530

    jdhjdb

--- capstone-project/src/component/Pages/Chicago.jsx
+++ capstone-project/src/component/Pages/Chicago.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 export default function Chicago() {
   return (
-    <div style={{marginTop:"220px", textAlign:"center", height:"300px"}}>
+    <div style={{marginTop:"160px", textAlign:"center", height:"300px"}}>
       <h1>Little Lemon Chicago Branch</h1>
       <h1 style={{marginTop:"20px"}}>Page Under Development</h1>
       <div>
             <Link to="/">
-                  <button className="main2button" >Switch To Home Page</button>
+                  <button className="main2button" >Switch to Home Page</button>
             </Link>
         </div>
       </div>
--- capstone-project/src/component/Pages/Contact.jsx
+++ capstone-project/src/component/Pages/Contact.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 export default function Contact() {
   return (
-    <div style={{marginTop:"220px", textAlign:"center", height:"300px"}}>
+    <div style={{marginTop:"160px", textAlign:"center", height:"300px"}}>
       <h1>Contact Us</h1>
       <h1 style={{marginTop:"14px"}}>Page Under Development</h1>
       <div>
             <Link to="/">
-                  <button className="main2button" >Switch To Home Page</button>
+                  <button className="main2button" >Switch to Home Page</button>
             </Link>
         </div>
       </div>
